caf886b095f14ed7d9e06ef599b0187c1b434701
Elaine Lin <contact.elainelin@gmail.com>
2020-12-22 18:37:51 -0600

    fixing file path after Folder Reorganization

--- 2020/solutions/python/Day_4.py
+++ 2020/solutions/python/Day_4.py
@@ -10,7 +10,7 @@
 import re
 from pathlib import Path
 
-base_path = Path(__file__).parent
+base_path = Path(__file__).parent.parent
 file_path = (base_path / '../inputs/Day_4.txt').resolve()
 
 aoc_input = ''
