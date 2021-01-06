4cf1afa7360ef5e18c00cbd625dd9b7c98ded9c2
Elaine Lin <contact.elainelin@gmail.com>
2021-01-05 23:39:52 -0600

    2015 Day 1 Part 1 Python
    Co-Authored-By: elainelin-dev <8635976+elainelin-dev@users.noreply.github.com>

---
+++ 2015/solutions/python/Day_01.py
@@ -0,0 +1,26 @@
+#---------------------------------------------
+# Author:       Elaine Lin
+# Created Date: 2020-12-19
+# References Used:
+#---------------------------------------------
+
+from pathlib import Path
+
+base_path = Path(__file__).parent.parent
+file_path = (base_path / '../inputs/Day_01.txt').resolve()
+
+aoc_input = []
+
+with open(file_path) as file:
+    aoc_input = file.read()
+
+#---------------------------------------------
+# Part 1
+#---------------------------------------------
+answer = (aoc_input.count('(')) - (aoc_input.count(')'))
+
+print ('Part 1 Answer:', answer)
+
+#---------------------------------------------
+# Part 2
+#---------------------------------------------
