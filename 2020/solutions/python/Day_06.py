ae4ea0d68c7c4fbbb0d61624eeb88b847424918f
elainelin-dev <elaine.lin@bba11.mccombs.utexas.edu>
2021-03-31 22:42:39 -0500

    Code cleanup 2020 Day 6 python

--- 2020/solutions/python/Day_06.py
+++ 2020/solutions/python/Day_06.py
@@ -7,17 +7,19 @@
 #   https://www.tutorialspoint.com/find-common-elements-in-list-of-lists-in-python
 #---------------------------------------------
 
+import os
 from pathlib import Path
 
+file_name = ((os.path.basename(__file__)).split('.')[0])
 base_path = Path(__file__).parent.parent
-file_path = (base_path / '../inputs/Day_06.txt').resolve()
+file_path = (base_path / '../inputs/{0}.txt'.format(file_name)).resolve()
 
-aoc_input = []
+aoc_input = None
 
 with open(file_path) as file:
-    aoc_input = file.read()
-		
-aoc_input = aoc_input.split('\n')
+    aoc_input = [line.strip() for line in file]
+
+aoc_input.append('')
 
 #---------------------------------------------
 # Part 1
@@ -26,15 +28,11 @@ count_total = 0
 s = ''
 
 for i in aoc_input:
-    if i == "":
+    if i == '':
         count_total += len(set(s))
         s = ''
     else:
         s += i
-        
-#Checking last group (last group will not have newline and therefore would not hit the 'if i == ""' logic above)         
-count_total += len(set(s))
-s = ''
         
 print ('Part 1 Answer:', count_total)
 
@@ -45,15 +43,10 @@ count_total = 0
 s = []
 
 for i in aoc_input:
-    if i == "":
+    if i == '':
         count_total += len(list(set.intersection(*map(set, s))))
         s = []
     else:
         s.append(list(i))
 
-#Checking last group (last group will not have newline and therefore would not hit the 'if i == ""' logic above) 
-count_total += len(list(set.intersection(*map(set, s))))
-s = []      
-
 print ('Part 2 Answer:', count_total)
-
