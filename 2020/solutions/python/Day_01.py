9888a5abf90cd962c48008a05387172b0fdbd069
Elaine Lin <contact.elainelin@gmail.com>
2021-09-23 00:27:16 -0500

    Code cleanup 2020 Day 1 python

--- 2020/solutions/python/Day_01.py
+++ 2020/solutions/python/Day_01.py
@@ -5,49 +5,30 @@
 #   https://stackoverflow.com/questions/40416072/reading-file-using-relative-path-in-python-project
 #   https://stackoverflow.com/questions/464864/how-to-get-all-possible-combinations-of-a-list-s-elements
 #   https://stackoverflow.com/questions/3925614/how-do-you-read-a-file-into-a-list-in-python
+#   https://stackoverflow.com/questions/13840379/how-can-i-multiply-all-items-in-a-list-together-with-python
 #---------------------------------------------
 
-from pathlib import Path
 import itertools
+import numpy as np
+import os
+from pathlib import Path
 
+file_name = ((os.path.basename(__file__)).split('.')[0])
 base_path = Path(__file__).parent.parent
-file_path = (base_path / '../inputs/Day_01.txt').resolve()
+file_path = (base_path / '../inputs/{0}.txt'.format(file_name)).resolve()
 
-aoc_input = []
-
-with open(file_path) as file:
-    for line in file:
-        aoc_input.append(int(line))
+aoc_input = [int(line) for line in open(file_path).read().split('\n')]
 
 #---------------------------------------------
 # Part 1
 #---------------------------------------------
-entry1 = None
-entry2 = None
-answer = None
-for i in (list(itertools.combinations(aoc_input, 2))):
-    if (i[0]+i[1])==2020:
-        entry1 = i[0]
-        entry2 = i[1]
-        answer = (i[0]*i[1])
-        break
+answer_1 = sum([np.prod(i) for i in itertools.combinations(aoc_input, 2) if (i[0]+i[1])==2020])
 
-print ('Part 1 Answer:', answer)
+print ('Part 1 Answer:', answer_1)
         
 #---------------------------------------------
 # Part 2
 #---------------------------------------------
-entry1 = None
-entry2 = None
-entry3 = None
-answer = None
-for i in (list(itertools.combinations(aoc_input, 3))):
-    if (i[0]+i[1]+i[2])==2020:
-        entry1 = i[0]
-        entry2 = i[1]
-        entry3 = i[2]
-        answer = (i[0]*i[1]*i[2])
-        break
+answer_2 = sum([np.prod(i) for i in itertools.combinations(aoc_input, 3) if (i[0]+i[1]+i[2])==2020])
 
-print ('Part 2 Answer:', answer)
-        
+print ('Part 2 Answer:', answer_2)
