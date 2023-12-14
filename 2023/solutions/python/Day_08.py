22d5c1f4f350244a84667d729b05e1fbf1abbd25
elainelin-dev <contact.elainelin@gmail.com>
2023-12-13 19:41:05 -0600

    refactor/consolidate day 8 logic

--- 2023/solutions/python/Day_08.py
+++ 2023/solutions/python/Day_08.py
@@ -32,25 +32,14 @@ for i, line in enumerate(raw_input):
 # print (f"{instructions = }")
 # print (f"{network_map = }")
 
-def solve_part_one(counter: int, key: str):
+def solve(part: int, counter: int, key: str):
     i = counter%len(instructions)
     key = network_map[key][0 if instructions[i]=='L' else 1]
     # print (f"{counter = } | {i = } | {instructions[i] = } | {key = }")
-    if key == 'ZZZ':
+    if (part==1 and key=='ZZZ') or (part==2 and key.endswith('Z')):
         return counter+1
-    return solve_part_one(counter=counter+1, key=key)
-
-def solve_part_two(counter: int, keys: list, d: dict):
-    i = counter%len(instructions)
-    keys = [network_map[key][0 if instructions[i]=='L' else 1] for key in keys]
-    # print (f"{counter = } | {i = } | {instructions[i] = } | {keys = }")
-    [d.update({i: counter+1}) for i, k in enumerate(keys) if k.endswith('Z') and d[i]==0]
-    if all([bool(v) for k, v in d.items()]):
-        return lcm(*list(d.values()))
-    return solve_part_two(counter=counter+1, keys=keys, d=d)
+    return solve(part=part, counter=counter+1, key=key)
 
 sys.setrecursionlimit(100000)
-print (f"answer: {solve_part_one(counter=0, key='AAA')}")
-
-a_keys = [k for k in network_map.keys() if k.endswith('A')]
-print (f"answer: {solve_part_two(counter=0, keys=a_keys, d={i:0 for i in range(len(a_keys))})}")
+print (f"answer: {solve(part=1, counter=0, key='AAA')}")
+print (f"answer: {lcm(*[solve(part=2, counter=0, key=k) for k in network_map.keys() if k.endswith('A')])}")
